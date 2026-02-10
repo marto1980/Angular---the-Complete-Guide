8d4a487d56533c12828628cbb947f4bbc534e8b6
Martin Ivanov <marto1980@gmail.com>
2026-02-10 15:12:30 +0200

    Revert "Trigger an ExpressionChangedAfterItHasBeenCheckedError"
    This reverts commit 321b44e91c6fd35b67ec141ffed8d04b71302099.

--- change-detection/src/app/info-message/info-message.component.ts
+++ change-detection/src/app/info-message/info-message.component.ts
@@ -11,8 +11,7 @@ export class InfoMessageComponent {
   get debugOutput() {
     console.log('[InfoMessages] "debugOutput" binding re-evaluated.')
 
-    // return 'InfoMessage Component Debug Output'
-    return Date.now()
+    return 'InfoMessage Component Debug Output'
   }
 
   onLog() {
