ba31c5cff0dacff54ee5a3c8bb94a0dcd0ff48b3
Martin Ivanov <marto1980@gmail.com>
2026-02-10 14:13:07 +0200

    Initialize the change-detection App

--- change-detection/src/app/app.ts
+++ change-detection/src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+
+import { CounterComponent } from './counter/counter.component'
+import { MessagesComponent } from './messages/messages.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  imports: [CounterComponent, MessagesComponent],
 })
 export class App {
-  protected readonly title = signal('change-detection');
+  get debugOutput() {
+    console.log('[AppComponent] "debugOutput" binding re-evaluated.')
+
+    return 'AppComponent Component Debug Output'
+  }
 }
