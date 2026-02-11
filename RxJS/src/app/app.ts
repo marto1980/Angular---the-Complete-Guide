d4ea9c971dd36ea8470b7f8aff37cf3d717f06dd
Martin Ivanov <marto1980@gmail.com>
2026-02-11 17:37:32 +0200

    Creating and Using an Observable

--- RxJS/src/app/app.ts
+++ RxJS/src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core'
+import { interval } from 'rxjs'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('RxJS');
+export class App implements OnInit {
+  private readonly destroyRef = inject(DestroyRef)
+
+  ngOnInit(): void {
+    const subscription = interval(1000).subscribe({
+      next: (value) => {
+        console.log(value)
+      },
+    })
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe()
+    })
+  }
 }
