290fb4a8555bc06b52d5cb07002ee5b7c0fd2037
Martin Ivanov <marto1980@gmail.com>
2026-02-11 18:48:26 +0200

    Solution without effect()

--- RxJS/src/app/app.ts
+++ RxJS/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core'
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core'
 import { interval, map } from 'rxjs'
 
 @Component({
@@ -10,9 +10,10 @@ export class App implements OnInit {
   private readonly destroyRef = inject(DestroyRef)
 
   constructor() {
-    effect(() => {
-      console.log(`Clicked button ${this.clickCount().toString()} times.`)
-    })
+    // effect(() => {
+    //   console.log(`Clicked button ${this.clickCount().toString()} times.`)
+    // })
+    console.log(`Clicked ${this.clickCount().toString()} number of times.`)
   }
   ngOnInit(): void {
     const subscription = interval(1000)
@@ -30,6 +31,6 @@ export class App implements OnInit {
   clickCount = signal(0)
   onClick() {
     this.clickCount.update((prevCount) => prevCount + 1)
-    // console.log(`Clicked ${this.clickCount().toString()} number of times.`)
+    console.log(`Clicked ${this.clickCount().toString()} number of times.`)
   }
 }
