e864a8cde3fbf135a9f2e0beced837b54599e407
Martin Ivanov <marto1980@gmail.com>
2026-02-13 10:22:46 +0200

    Restore Client Hydration with Event Replay

--- httpRequests/frontend/src/app/app.config.ts
+++ httpRequests/frontend/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { provideHttpClient, withFetch } from '@angular/common/http'
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core'
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser'
 import { provideRouter } from '@angular/router'
 
 import { routes } from './app.routes'
@@ -8,7 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    // provideClientHydration(withEventReplay()), // this prevents showing the loading fallback
+    provideClientHydration(withEventReplay()), // this prevents showing the loading fallback
     provideHttpClient(withFetch()),
   ],
 }
