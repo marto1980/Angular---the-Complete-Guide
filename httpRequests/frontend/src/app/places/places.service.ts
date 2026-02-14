fcaf179b2ae129f4defb819c235148ec10ffa06f
Martin Ivanov <marto1980@gmail.com>
2026-02-14 12:10:15 +0200

    Improve Optimistic Updating

--- httpRequests/frontend/src/app/places/places.service.ts
+++ httpRequests/frontend/src/app/places/places.service.ts
@@ -46,11 +46,23 @@ export class PlacesService {
   }
 
   addPlaceToUserPlaces(place: Readonly<Place>) {
-    this.userPlaces.update((prevPlaces) => [...prevPlaces, place])
+    const prevPlaces = this.userPlaces()
 
-    return this.httpClient.put('http://localhost:3000/user-places', {
-      placeId: place.id,
-    })
+    if (!prevPlaces.some((prevPlace) => prevPlace.id === place.id)) {
+      this.userPlaces.set([...prevPlaces, place])
+    }
+
+    return this.httpClient
+      .put('http://localhost:3000/user-places', {
+        placeId: place.id,
+      })
+      .pipe(
+        catchError(() => {
+          this.userPlaces.set(prevPlaces)
+
+          return throwError(() => new Error('Failed to store selected place.'))
+        }),
+      )
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
