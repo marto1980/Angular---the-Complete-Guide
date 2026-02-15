aeb3631d08a2179d1d2234d8c0c34b517f52791e
Martin Ivanov <marto1980@gmail.com>
2026-02-15 15:10:37 +0200

    Initialise Forms App

--- forms/src/app/app.ts
+++ forms/src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+
+import { LoginComponent } from './auth/login/login.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LoginComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('forms');
-}
+export class App {}
