fdcf098801551daa75213daa2ed7efda4f158b7f
Martin Ivanov <marto1980@gmail.com>
2026-02-16 15:39:11 +0200

    Reset Login Component

--- forms/src/app/auth/login/login.component.ts
+++ forms/src/app/auth/login/login.component.ts
@@ -1,57 +1,9 @@
-import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core'
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
-import { FormsModule, NgForm } from '@angular/forms'
-import { debounceTime } from 'rxjs'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
-  form = viewChild<NgForm>('form')
-  destroyRef = inject(DestroyRef)
-
-  constructor() {
-    afterNextRender(() => {
-      const savedForm = globalThis.localStorage.getItem('saved-login-form')
-
-      if (savedForm) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        const loadedFormData = JSON.parse(savedForm)
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-        const savedEmail = loadedFormData.email
-
-        setTimeout(() => {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-          this.form()?.setValue({ email: savedEmail, password: '' })
-        }, 1)
-      }
-
-      this.form()
-        ?.valueChanges?.pipe(debounceTime(500))
-        .pipe(takeUntilDestroyed(this.destroyRef))
-        .subscribe({
-          next: (value) => {
-            globalThis.localStorage.setItem(
-              'saved-login-form',
-              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-              JSON.stringify({ email: value.email }),
-            )
-          },
-        })
-    })
-  }
-
-  onSubmit(formData: Readonly<NgForm>) {
-    console.log(formData)
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const { email: enteredEmail, password: enteredPassword } = formData.form.value
-    console.log(enteredEmail, enteredPassword)
-    if (formData.form.invalid) {
-      return
-    }
-    formData.form.reset()
-  }
-}
+export class LoginComponent {}
