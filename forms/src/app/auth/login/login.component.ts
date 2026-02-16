7786c5e9e5dddb8217aa9ef174846174f4856673
Martin Ivanov <marto1980@gmail.com>
2026-02-16 10:32:06 +0200

    Get Access to Angular-managed Form

--- forms/src/app/auth/login/login.component.ts
+++ forms/src/app/auth/login/login.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core'
-import { FormsModule } from '@angular/forms'
+import { FormsModule, NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
   imports: [FormsModule],
-  standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  onSubmit(form: Readonly<NgForm>) {
+    console.log(form)
+  }
+}
