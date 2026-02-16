88deee918facacb51538db52a8ed1f7034028b0e
Martin Ivanov <marto1980@gmail.com>
2026-02-16 16:01:55 +0200

    Get Started with Reactive Forms

--- forms/src/app/auth/login/login.component.ts
+++ forms/src/app/auth/login/login.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core'
+import { FormControl, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  })
+
+  onSubmit() {
+    /* empty */
+  }
+}
--- forms/src/app/auth/signup/signup.component.ts
+++ forms/src/app/auth/signup/signup.component.ts
@@ -4,6 +4,6 @@ import { Component } from '@angular/core'
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css',
+  styleUrl: './signup.component.scss',
 })
 export class SignupComponent {}
