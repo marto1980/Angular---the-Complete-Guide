ad19431f1c2fcbd431c2c16380feac7a14eab435
Martin Ivanov <marto1980@gmail.com>
2026-02-17 11:29:01 +0200

    Use the signup Form and register the Email & Password Fields

--- forms/src/app/app.ts
+++ forms/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core'
 
-import { LoginComponent } from './auth/login/login.component'
+import { SignupComponent } from './auth/signup/signup.component'
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent],
+  imports: [SignupComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- forms/src/app/auth/signup/signup.component.ts
+++ forms/src/app/auth/signup/signup.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core'
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-signup',
+  imports: [ReactiveFormsModule],
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  })
+
+  onSubmit() {
+    console.log(this.form)
+  }
+
+  onReset() {
+    this.form.reset()
+  }
+}
