729597d2dce1bf0ebb80ce4ea622f86cd411c41e
Martin Ivanov <marto1980@gmail.com>
2026-02-19 10:18:03 +0200

    Set Query Parameters

--- routing/src/app/tasks/tasks.component.ts
+++ routing/src/app/tasks/tasks.component.ts
@@ -1,14 +1,15 @@
 import { Component, computed, inject, input } from '@angular/core'
+import { RouterLink } from '@angular/router'
 
 import { TaskComponent } from './task/task.component'
 import { TasksService } from './tasks.service'
 
 @Component({
   selector: 'app-tasks',
+  imports: [RouterLink, TaskComponent],
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
 })
 export class TasksComponent {
   private readonly tasksService = inject(TasksService)
