8bca2a7ddbb31f7f104f7828db9164324ca69d2e
Martin Ivanov <marto1980@gmail.com>
2026-02-23 17:05:43 +0200

    Configure and Use Static Site Generation

--- deploying/src/app/app.routes.server.ts
+++ deploying/src/app/app.routes.server.ts
@@ -3,7 +3,18 @@ import { RenderMode, ServerRoute } from '@angular/ssr'
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'users/:userId/**',
-    renderMode: RenderMode.Server,
+    getPrerenderParams: () =>
+      Promise.resolve([
+        {
+          userId: 'u1',
+        },
+        { userId: 'u2' },
+        { userId: 'u3' },
+        { userId: 'u4' },
+        { userId: 'u5' },
+        { userId: 'u6' },
+      ]),
+    renderMode: RenderMode.Prerender,
   },
   {
     path: '**',
